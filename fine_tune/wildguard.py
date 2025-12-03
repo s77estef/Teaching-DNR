eb82c933b83ec630454d0caf127df641844bd3bb
s77estef <s77estef@uni-bonn.de>
2025-12-03 18:02:23 +0100

    .

--- fine_tune/wildguard.py
+++ fine_tune/wildguard.py
@@ -2,11 +2,11 @@ from datasets import load_dataset
 
 # features: ['prompt', 'adversarial', 'response', 'prompt_harm_label', 'response_refusal_label', 'response_harm_label', 'subcategory']
 # num_rows: 86759
-train = load_dataset('allenai/wildguardmix', 'wildguardtrain')["train"]
+train = load_dataset('allenai/wildguardmix', 'wildguardtrain', split=f"train[:100%]")
 
 # features: ['prompt', 'response', 'adversarial', 'prompt_harm_label', 'response_refusal_agreement', 'response_refusal_label', 'response_harm_label', 'subcategory', 'prompt_harm_agreement', 'response_harm_agreement']
 # num_rows: 1725
-test = load_dataset('allenai/wildguardmix', 'wildguardtest')["test"]
+test = load_dataset('allenai/wildguardmix', 'wildguardtest', split=f"test[:100%]")
 
 print(test)
 
